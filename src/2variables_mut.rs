e8e0defceffd56712245e1c1e5207740f787491c
TAKZUIN <Takzuin@gmail.com>
2025-03-21 03:11:09 -0500

    Update

---
+++ src/2variables_mut.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let x = 8;
+    //x = 9; // This will cause an error
+    let mut y = 8;
+    y = 9; // This is fine
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
---
+++ src/4variable_types.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let x = 8; // This is an integer
+    let y = 8.0; // This is a float
+    let z = true; // This is a boolean
+    let a = 'a'; // This is a character
+    let b = "Hello"; // This is a string
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
+    println!("The value of a is: {}", a);
+    println!("The value of b is: {}", b);
+    let x: i32 = 8; // This is an integer
+}
